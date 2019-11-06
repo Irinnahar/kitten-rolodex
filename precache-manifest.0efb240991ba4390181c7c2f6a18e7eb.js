4b2d948c39d7aaa7e58f88ce0a7800cc2e8ee3c5
Irinnahar <irincse.bu@gmail.com>
2019-11-06 23:08:15 +0600

    Updates

---
+++ precache-manifest.0efb240991ba4390181c7c2f6a18e7eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac03bd441077c50f09598f4415cc81a2",
+    "url": "/kitten-rolodex/index.html"
+  },
+  {
+    "revision": "d2dcbde4e6a96e6dfc75",
+    "url": "/kitten-rolodex/static/css/main.d0bf5dcc.chunk.css"
+  },
+  {
+    "revision": "a20d80494dfc80982286",
+    "url": "/kitten-rolodex/static/js/2.d38236e0.chunk.js"
+  },
+  {
+    "revision": "d2dcbde4e6a96e6dfc75",
+    "url": "/kitten-rolodex/static/js/main.2c89239f.chunk.js"
+  },
+  {
+    "revision": "5a2b6b6a2ebb8fbbad4c",
+    "url": "/kitten-rolodex/static/js/runtime-main.57cb85e9.js"
+  }
+]);
